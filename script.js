16cbb62754a8fa6190c712545b07c908ab357b4c
Jennifer <jenniferpguzman0@gmail.com>
2024-12-02 13:24:27 -0500

    more updates

---
+++ script.js
@@ -0,0 +1,25 @@
+// class car {
+//     constructor(make,model,year){
+//         this.make = make;
+//         this.model = model;
+//         this.year = year;
+//     }
+//     describe(){
+//         return `${this.year} ${this.make} ${this.model}`;
+//     }
+// }
+
+// const myCar = new car("Toyota","Corolla",2020);
+// console.log(myCar.describe());
+
+
+// 1. Store the value in an array
+// 2. Create a for loop 
+// 3. iterate through the array for every number in the array, print it 
+// 4. if multiple of 3 print "fizz" (find multpile by dividing by 3 and if there is no remainder, it is a multiple)
+// 5. if else multiple of 5 print "buzz"
+// 6.if else multiple of 3 and 5 print "fizzbuzz"
+
+num = {1,2,3,4,5,6,7,8,9,10,11,12,13}
+
+for(let i === 0, )
